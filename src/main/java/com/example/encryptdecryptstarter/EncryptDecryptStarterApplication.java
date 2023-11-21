bd953c93aa1193c4f6a5feb6366bccc34ff3ab84
zj <2099487761@qq.com>
2023-11-21 19:19:16 +0800

    init

---
+++ src/main/java/com/example/encryptdecryptstarter/EncryptDecryptStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.encryptdecryptstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptDecryptStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EncryptDecryptStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/encryptdecryptstarter/EncryptDecryptStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.encryptdecryptstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptDecryptStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
